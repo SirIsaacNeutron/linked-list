a112bee52cadb1b09753c06b59b408a0d2c25b7e
SirIsaacNeutron <dbadir@cox.net>
2023-03-20 01:32:18 -0700

    Add several LinkedList methods

---
+++ linkedList.js
@@ -0,0 +1,91 @@
+export class LinkedList {
+    constructor() {
+        this.head = null
+        this.length = 0
+    }
+
+    append = value => {
+        const newNode = new ListNode(value)
+
+        if (this.head === null) {
+            this.head = newNode
+        }
+        else {
+            let currentNode = this.head
+            while (currentNode.next != null) {
+                currentNode = currentNode.next
+            }
+
+            currentNode.next = newNode
+        }
+        ++this.length
+    }
+
+    prepend = value => {
+        const newNode = new ListNode(value, this.head)
+        this.head = newNode
+
+        ++this.length
+    }
+
+    tail = () => {
+        let currentNode = this.head
+        while (currentNode.next !== null) {
+            currentNode = currentNode.next
+        }
+
+        return currentNode
+    }
+
+    at = index => {
+        let currentNode = this.head
+        let currentIndex = 0
+        while (currentIndex < this.length) {
+            if (currentIndex === index) {
+                return currentNode
+            }
+            ++currentIndex
+            currentNode = currentNode.next
+        }
+        return -1
+    }
+
+    
+    pop = () => {
+        let currentNode = this.head
+        while (currentNode.next !== null) {
+            const nextNode = currentNode.next
+            // If the next node is the last node
+            if (nextNode.next === null) {
+                currentNode.next = null
+                return nextNode
+            }
+            currentNode = nextNode
+        }
+        const oldHead = this.head
+        this.head = null
+        return oldHead
+    }
+
+    size = () => {
+        return this.length
+    }
+
+    toString = () => {
+        let currentNode = this.head
+        let string = ""
+        while (currentNode != null) {
+            string += `( ${currentNode.value} ) -> `
+            currentNode = currentNode.next
+        }
+        string += "null"
+        return string
+    }
+}
+
+class ListNode {
+    constructor(value, next = null) {
+        this.value = value
+        this.next = next
+    }
+}
---
+++ linkedList.test.js
@@ -0,0 +1,78 @@
+import { LinkedList } from "./linkedList";
+
+test("is initially size 0", () => {
+    expect(new LinkedList().size()).toEqual(0)
+})
+
+test("appending elements", () => {
+    const list = new LinkedList()
+
+    list.append(1)
+
+    expect(list.toString()).toEqual("( 1 ) -> null")
+    expect(list.size()).toEqual(1)
+
+    list.append(2)
+    list.append(3)
+    expect(list.toString()).toEqual("( 1 ) -> ( 2 ) -> ( 3 ) -> null")
+    expect(list.size()).toEqual(3)
+})
+
+test("prepending elements", () => {
+    const list = new LinkedList()
+
+    list.prepend(1)
+
+    expect(list.toString()).toEqual("( 1 ) -> null")
+    expect(list.size()).toEqual(1)
+
+    list.prepend(0)
+    expect(list.toString()).toEqual("( 0 ) -> ( 1 ) -> null")
+    expect(list.size()).toEqual(2)
+})
+
+test("returning tail nodes", () => {
+    const list = new LinkedList()
+
+    list.prepend(1)
+
+    expect(list.tail().value).toEqual(1)
+
+    list.append(2)
+    expect(list.tail().value).toEqual(2)
+})
+
+test("using at() to access elements", () => {
+    const list = new LinkedList()
+
+    expect(list.at(0)).toEqual(-1)
+    expect(list.at(1)).toEqual(-1)
+    expect(list.at(-1)).toEqual(-1)
+
+    list.prepend(1)
+
+    expect(list.at(0).value).toEqual(1)
+
+    list.append(2)
+    list.append(3)
+
+    expect(list.at(1).value).toEqual(2)
+    expect(list.at(2).value).toEqual(3)
+
+    expect(list.at(3)).toEqual(-1)
+})
+
+test("popping elements", () => {
+    const list = new LinkedList()
+
+    list.prepend(1)
+
+    expect(list.pop().value).toEqual(1)
+    expect(list.toString()).toEqual("null")
+
+    list.append(1)
+    list.append(2)
+
+    expect(list.pop().value).toEqual(2)
+    expect(list.toString()).toEqual("( 1 ) -> null")
+})
